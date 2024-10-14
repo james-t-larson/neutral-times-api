92baa3570649ae9388ac5892de4e957eb4e88e3e
James Larson <timothy_james@tuta.io>
2024-10-14 14:10:07 -0700

    test: remove unused tests from StoriesControllerTest and StoriesTest
    - Removed `new`, `create`, `edit`, `update`, and `destroy` action tests
    from `StoriesControllerTest`
    - Removed corresponding system tests for `create`, `update`, and
    `destroy` in `StoriesTest`
    - These actions are no longer relevant, and the associated tests have
    been cleaned up for better maintenance

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -10,39 +10,8 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_story_url
-    assert_response :success
-  end
-
-  test "should create story" do
-    assert_difference("Story.count") do
-      post stories_url, params: { story: { published_at: @story.published_at, summary: @story.summary, title: @story.title } }
-    end
-
-    assert_redirected_to story_url(Story.last)
-  end
-
   test "should show story" do
     get story_url(@story)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_story_url(@story)
-    assert_response :success
-  end
-
-  test "should update story" do
-    patch story_url(@story), params: { story: { published_at: @story.published_at, summary: @story.summary, title: @story.title } }
-    assert_redirected_to story_url(@story)
-  end
-
-  test "should destroy story" do
-    assert_difference("Story.count", -1) do
-      delete story_url(@story)
-    end
-
-    assert_redirected_to stories_url
   end
 end
